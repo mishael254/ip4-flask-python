e0b3aad3d4bcd02bfd4000e639d7c16f46925982
mishael254 <mishaelmishantoz@gmail.com>
2020-07-22 22:56:57 +0300

    create a new config file and import os create class config  and define secretkey as variables

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://antavio:1955@localhost/blogs'
+    DEBUG = True
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
